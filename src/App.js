23ae42ceb8695202af4327e236be603726dca963
Nick Hamnett <nick.hamnett@sait.ca>
2022-04-03 21:31:52 -0600

    Added portfolio components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
 import './App.css';
 
+import Header from './Header.js';
+import Education from './Education';
+import TechnologySummary from './TechnologySummary';
+import WorkExperience from './WorkExperience';
+
 function App() {
+    return (
+        <div className="App">
+            <Header />
+            
+            <main className="container">
+                <div className="row mt-3">
+                    <div className="col-md">
+                        <WorkExperience />
+                    </div>
+                    <div className="col-md">
+                        <Education />
+                    </div>
+                </div>
+                <div className="row mt-3">
+                    <div className="col-md mb-5">
+                        <TechnologySummary />
+                    </div>
+                </div>
+            </main>
+        </div>
+    );
 }
 
 export default App;
